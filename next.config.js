37032eaae4e8373fad3c467f79ae5253ea29bfb0
JacobiClark <jacobcs@zoho.com>
2021-04-16 12:13:03 -0700

    made target serverless in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
-    FINNHUB_API_KEY: 'c1ip26748v6vit216ok0',
-    YAHOO_FINANCE_KEY: 'fcc6dc3e20msh93b470760f15126p1d15cajsnec3c508f9f4f'
+    FINNHUB_API_KEY: "c1ip26748v6vit216ok0",
+    YAHOO_FINANCE_KEY: "fcc6dc3e20msh93b470760f15126p1d15cajsnec3c508f9f4f",
   },
-}
+  target: "serverless",
+};
